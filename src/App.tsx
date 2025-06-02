c009410fc43f5d556a21cc29dc5d3746e58425fe
arvee0220 <rowellverdejo@gmail.com>
2025-06-02 15:42:49 +0800

    Add r3f-perf dependency for monitoring perfomance

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-// import Group from "./scene/Group";
 import * as THREE from "three";
+// import Group from "./scene/Group";
 // import Custom from "./scene/CustomShape";
 import Particles from "./scene/Particles";
 import PredatorModel from "./scene/PredatorModel";
 import { Suspense } from "react";
 import Loader from "./loader/Loader";
+import { Perf } from "r3f-perf";
 
 export default function App() {
     const canvasHandler = (state: { gl: THREE.WebGLRenderer }) => {
@@ -16,6 +17,7 @@ export default function App() {
 
     return (
         <Canvas className="canvas" /* gl={{ alpha: true }} */ camera={{ position: [0, 0, 5], fov: 75, near: 0.1, far: 100 }} onCreated={canvasHandler}>
+            <Perf position="top-left"/>
             <OrbitControls />
             {/* <Group /> */}
             {/* <Custom/> */}
